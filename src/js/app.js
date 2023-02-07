47ded919af2e80b5762604cbbdade97431580338
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-02-07 03:50:25 +0200

    section work add js block create

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,10 @@
 import header from "../parts/header/header.js";
 import services from "../parts/services/services.js";
 import news from "../parts/news/news.js";
-
+import work from "../parts/work/work.js";
 document.addEventListener('DOMContentLoaded', () => {
     header();
+    work();
     services();
     news();
 })
