030951f1ad28b4e78bccb828ee34d7b6a30191f7
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-02-11 16:30:02 +0200

    add chenge big photo in slider

--- src/parts/about/about.js
+++ src/parts/about/about.js
@@ -1,18 +1,15 @@
 export default function about() {
 
     const item = document.querySelector(".slide-item")
+    const items = document.querySelectorAll('.slide-item')
     const sliderLine = document.querySelector('.about__slaider-card-list') 
     const countElements = sliderLine.childElementCount;
-    
     let offsetSize = item.getBoundingClientRect().width;
     let offset = 0;
     let sizeVewline = (countElements * offsetSize)-(offsetSize * 4);
-    
     const btnPre = document.querySelector('.slide-btn-pre');
     const btnNext = document.querySelector('.slide-btn-next');
-
     
-
     btnNext.addEventListener('click', () => {
         offset -= offsetSize;
 
@@ -29,4 +26,28 @@ export default function about() {
         }
         sliderLine.style.left = `${offset}px`;
     })
+
+    // ====================================
+  
+    const photos = document.querySelectorAll('.photo-min')
+    const bigImg = document.querySelector('.big-img')
+    
+    setBigPhoto();
+
+    items.forEach((item, i) => {
+        item.addEventListener('mouseenter', () => {
+            photos[i].style.top = '-15px';
+            setBigPhoto(i);
+        })
+        item.addEventListener('mouseleave', () => {
+
+            photos[i].style.top = '0';
+        })
+    })
+
+    function setBigPhoto(i = 0) {
+            bigImg.src =`../img/user-${i}.jpg`;
+    }
+
+
 }
