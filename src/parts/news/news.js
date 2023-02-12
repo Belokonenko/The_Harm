cede5301d8ca7b6dd0cbd50b2f9ddc13468c9943
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-02-12 11:55:27 +0200

    fix news"

--- src/parts/news/news.js
+++ src/parts/news/news.js
@@ -1,14 +1,16 @@
 export default function news() {
     const items = document.querySelectorAll('.news-item');
     const dateBlock = document.querySelectorAll('.news-date-block');
-
+    const titles = document.querySelectorAll('.title-news')
     items.forEach((item, i) => {
         item.addEventListener('mouseover', () => {
             dateBlock[i].classList.add('news-date-block--hover')
+            titles[i].style.color = '#18CFAB';
         })
 
         item.addEventListener('mouseout', () => {
             dateBlock[i].classList.remove('news-date-block--hover')
+            titles[i].style.color = '#717171';
         })
         
     })
