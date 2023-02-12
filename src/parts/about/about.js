c7435bcf31916877e9761c63cdbd7cd2d4db7a5f
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-02-12 16:51:23 +0200

    slider big done

--- src/parts/about/about.js
+++ src/parts/about/about.js
@@ -1,53 +1,33 @@
 export default function about() {
-
-    const item = document.querySelector(".slide-item")
-    const items = document.querySelectorAll('.slide-item')
-    const sliderLine = document.querySelector('.about__slaider-card-list') 
-    const countElements = sliderLine.childElementCount;
-    let offsetSize = item.getBoundingClientRect().width;
-    let offset = 0;
-    let sizeVewline = (countElements * offsetSize)-(offsetSize * 4);
     const btnPre = document.querySelector('.slide-btn-pre');
     const btnNext = document.querySelector('.slide-btn-next');
     
-    btnNext.addEventListener('click', () => {
+    const slideLine = document.querySelector('.about__slaider-line');
+    const slideItems = document.querySelectorAll('.about__slaider-line-item')
+    const siideItemsCount = slideLine.childElementCount;
+    let offsetSize = slideItems[0].getBoundingClientRect().width;
+    let offset = 0;
+    let sizeVewline = siideItemsCount * offsetSize ;
+    
+    btnNext.addEventListener("click", () => {
+        
         offset -= offsetSize;
+        console.log(offset); 
 
-        if (offset < -sizeVewline) {
-           offset = 0; 
+        if (offset <= -sizeVewline) {
+            offset = 0;
         }
-        sliderLine.style.left = `${offset}px`;
-    })
-    
-    btnPre.addEventListener('click', () => {
+        slideLine.style.left = `${offset}px`;
+    });
+
+    btnPre.addEventListener("click", () => {
         offset += offsetSize;
-        if (offset > 0) {
-            offset = -sizeVewline;
+        console.log(offset); 
+        if (offset >0) {
+            offset = -(sizeVewline - offsetSize) ;
         }
-        sliderLine.style.left = `${offset}px`;
-    })
 
-    // ====================================
-  
-    const photos = document.querySelectorAll('.photo-min')
-    const bigImg = document.querySelector('.big-img')
-    
-    setBigPhoto();
-
-    items.forEach((item, i) => {
-        item.addEventListener('mouseenter', () => {
-            photos[i].style.top = '-15px';
-            setBigPhoto(i);
-        })
-        item.addEventListener('mouseleave', () => {
-
-            photos[i].style.top = '0';
-        })
-    })
-
-    function setBigPhoto(i = 0) {
-            bigImg.src =`../img/user-${i}.jpg`;
-    }
-
+        slideLine.style.left = `${offset}px`;
+    });
 
 }
