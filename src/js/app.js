0130c73db9824fc3a9a10f0080812e1e73606b67
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-02-12 09:32:47 +0200

    add btn add gallery

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,13 @@ import services from "../parts/services/services.js";
 import news from "../parts/news/news.js";
 import work from "../parts/work/work.js";
 import about from "../parts/about/about.js";
+import gallery from "../parts/gallery/gallery.js";
+
 document.addEventListener('DOMContentLoaded', () => {
     header();
     work();
     services();
     news();
     about();
+    gallery();
 })
---
+++ src/parts/gallery/gallery.js
@@ -0,0 +1,19 @@
+export default function gallery() {
+    const galleryImages = document.querySelector('.gallery__images');
+    const bntLoadPhoto  = document.querySelector('.gallery__btn-load-photo');
+    addGalleryPhotos();
+ 
+    bntLoadPhoto.addEventListener('click', () => {
+        addGalleryPhotos();
+    });
+    
+    function addGalleryPhotos() {
+        for (let index = 0; index < 8; index++) {
+            const item = document.createElement('li')
+            item.innerHTML =`
+            <li class="item"><img src="../img/gallery-${index}.jpg" alt=""></li>
+        `;
+            galleryImages.append(item);
+        }    
+    }
+}
