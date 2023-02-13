5999cd95fabda134363ce14a1e6d951581810489
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-02-13 11:42:07 +0200

    xxx

--- src/parts/about/about.js
+++ src/parts/about/about.js
@@ -1,18 +1,20 @@
 export default function about() {
-    const btnPre = document.querySelector('.slide-btn-pre');
-    const btnNext = document.querySelector('.slide-btn-next');
-    
-    const slideLine = document.querySelector('.about__slaider-line');
-    const slideItems = document.querySelectorAll('.about__slaider-line-item')
+    const btnPre = document.querySelector(".slide-btn-pre");
+    const btnNext = document.querySelector(".slide-btn-next");
+
+    const slideLine = document.querySelector(".about__slaider-line");
+    const slideItems = document.querySelectorAll(".about__slaider-line-item");
     const siideItemsCount = slideLine.childElementCount;
+
+    const itemsMinPhoto = document.querySelectorAll(".item-min-photo");
+
     let offsetSize = slideItems[0].getBoundingClientRect().width;
     let offset = 0;
-    let sizeVewline = siideItemsCount * offsetSize ;
-    
+    let sizeVewline = siideItemsCount * offsetSize;
+
     btnNext.addEventListener("click", () => {
-        
         offset -= offsetSize;
-        console.log(offset); 
+        console.log(offset);
 
         if (offset <= -sizeVewline) {
             offset = 0;
@@ -22,12 +24,31 @@ export default function about() {
 
     btnPre.addEventListener("click", () => {
         offset += offsetSize;
-        console.log(offset); 
-        if (offset >0) {
-            offset = -(sizeVewline - offsetSize) ;
+        console.log(offset);
+        if (offset > 0) {
+            offset = -(sizeVewline - offsetSize);
         }
 
         slideLine.style.left = `${offset}px`;
     });
 
+    activeSlide();
+    
+    itemsMinPhoto.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            console.log(i);
+            activeSlide(i);
+        })
+    })
+
+    function activeSlide(index = 0) {
+        itemsMinPhoto.forEach((item) => {
+            item.classList.remove('item-min-photo--active');
+        })
+
+        itemsMinPhoto[index].classList.add('item-min-photo--active');
+        
+        offset = -(offsetSize * index);
+        slideLine.style.left = `${offset}px`;
+    }
 }
