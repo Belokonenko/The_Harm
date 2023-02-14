6feaa3888a557481a2c92a1d7a86721028f687f3
Belokonenko.Viacheslav@gmail <Belokonenko.Viacheslav@gmail.com>
2023-02-14 07:18:38 +0200

    burger done

--- src/parts/header/burger.js
+++ src/parts/header/burger.js
@@ -1,9 +1,20 @@
 export default function burger() {
     const btnBurger = document.querySelector(".header__btn-burger");
     const nav = document.querySelector('.header__nav');
+
     
     btnBurger.addEventListener("click", () => {
         btnBurger.classList.toggle("header__btn-burger--active");
         nav.classList.toggle('header__nav--active');
     });
+    
+    nav.addEventListener('click', () => {
+        nav.classList.remove('header__nav--active');
+        btnBurger.classList.remove("header__btn-burger--active");
+    })
+
+    nav.addEventListener('mouseleave', () => {
+        nav.classList.remove('header__nav--active');
+        btnBurger.classList.remove("header__btn-burger--active");
+    })
 }
